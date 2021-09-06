d1afc17701b9505148f2b4b204dbafbbf2ca3b97
Anna Kotelnikova <anutka0306@mail.ru>
2021-09-06 09:27:38 +0300

    устранена горизонтальна полоса прокрутки

--- public/js/custom.js
+++ public/js/custom.js
@@ -13,6 +13,35 @@ $(document).ready(function () {
     $('.brands__list_slider').slick({
         infinite: true,
         slidesToShow: 4,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1366,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
 });
