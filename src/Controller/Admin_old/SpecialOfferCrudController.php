7e47de82a573b3aeba3c3836371eeabdba0d209a
Anna Kotelnikova <anutka0306@mail.ru>
2021-09-14 13:54:11 +0300

    акции сделаны редактируемыми

--- src/Controller/Admin_old/SpecialOfferCrudController.php
+++ src/Controller/Admin_old/SpecialOfferCrudController.php
@@ -8,7 +8,9 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class SpecialOfferCrudController extends AbstractCrudController
@@ -37,8 +39,9 @@ class SpecialOfferCrudController extends AbstractCrudController
             BooleanField::new('published', 'Активность'),
             NumberField::new('ordering','Позиция'),
             TextField::new('metaTitle','Title')->hideOnIndex(),
-            TextField::new('metaDescription', 'Description')->hideOnIndex(),
+            TextEditorField::new('metaDescription', 'Description')->hideOnIndex(),
             TextField::new('slug', 'Алиас'),
+            /*ImageField::new('image', 'Картинка акции')->setUploadDir('/public/img/offers/')->setBasePath('/img/offers/'),*/
             DateTimeField::new('modifyDate', 'Дата изменения')->hideOnIndex()
         ];
     }
