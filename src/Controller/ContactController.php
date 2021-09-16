9f8fae5321df11251d97463fd6400eaa6f445929
Anna Kotelnikova <anutka0306@mail.ru>
2021-09-16 13:12:40 +0300

    страница контакты

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -6,16 +6,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\ContentRepository;
+use App\Repository\MenuTopRepository;
+use App\Repository\MenuLeftRepository;
+use App\Repository\PriceBrandRepository;
 
 class ContactController extends AbstractController
 {
     /**
+     * @var PriceBrandRepository
+     */
+    protected $priceBrandRepository;
+    /**
      * @var ContentRepository
      */
     protected $page_repository;
-    public function __construct(ContentRepository $repository)
+
+    /**
+     * @var MenuTopRepository
+     */
+    protected $menuTopRepository;
+
+    /**
+     * @var MenuLeftRepository
+     */
+    protected $menuLeftRepository;
+
+    public function __construct(ContentRepository $repository, MenuTopRepository $menuTopRepository, MenuLeftRepository $menuLeftRepository, PriceBrandRepository $priceBrandRepository)
     {
         $this->page_repository = $repository;
+        $this->menuTopRepository = $menuTopRepository;
+        $this->menuLeftRepository = $menuLeftRepository;
+        $this->priceBrandRepository = $priceBrandRepository;
     }
 
     /**
@@ -26,8 +47,15 @@ class ContactController extends AbstractController
         if(! $page = $this->page_repository->findOneBy(['path'=>'/contacts/'])){
             throw $this->createNotFoundException('Page /contacts/ not found');
         }
+        $topMenu = $this->menuTopRepository->findAll();
+        $leftMenu = $this->menuLeftRepository->findAll();
+        $brands = $this->priceBrandRepository->findAll();
+
         return $this->render('v2/pages/contact/index.html.twig', [
             'page' => $page,
+            'brands' => $brands,
+            'topMenu' => $topMenu,
+            'leftMenu' => $leftMenu,
         ]);
     }
 }
