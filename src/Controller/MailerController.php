ada990db45ce71e109c249e1b111b7026bae7514
Anna Kotelnikova <anutka0306@mail.ru>
2021-09-23 20:40:13 +0300

    Update MailerController.php

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -126,7 +126,7 @@ class MailerController extends AbstractController
         $to = 'anya-programmist@qmotors.ru';
 
             $email = (new Email())
-                ->from('robot@piksp.ru')
+                ->from('info@my-side.online')
                 ->to((string)$to)
                 ->subject('Новая заявка с сайта Piksp.ru')
                 ->html('<p>Новая заявка с сайта Styled.cc</p>
