1ed5b9849184caf63f64c880308adb694c48091c
anutka0306 <anutka0306@mail.ru>
2022-01-31 00:09:56 +0300

    from host

--- src/Controller/MailerController.php
+++ src/Controller/MailerController.php
@@ -124,17 +124,6 @@ class MailerController extends AbstractController
      */
     public function callback_form(Request $request, MailerInterface $mailer){
 
-        //Begin roistat
-        $roistatFilePath = "{$_SERVER['DOCUMENT_ROOT']}/roistat/RoistatEvents.php";
-        if(is_file($roistatFilePath)) {
-            require_once $roistatFilePath;
-            try {
-                $event = new \RoistatEvents($_REQUEST, 'Заявка с формы piksp.ru');
-                $event->execute();
-            }catch (\Exception $exception) {}
-        }
-        //End roistat
-
         $token = "1737028189:AAEFd51Z6vSHslgX-CNMtItwWD6Iy5EIP74";
         $chat_id = "-1001408803296";# Заявки VAG-PIK
 
@@ -163,48 +152,6 @@ if (!$sendTextToTelegram){
             <p>Телефон отправителя: ' . $request->get('phone') . '</p>'
                 );
             $mailer->send($email);
-        return new JsonResponse(['success'=>'<p>Спасибо! Ваша заявка отправлена.</p>']);
-    }
-
-
-    /**
-     * @Route("/raschet_form", name="callback_form")
-     */
-    public function raschet_form(Request $request, MailerInterface $mailer){
-
-        $token = "1737028189:AAEFd51Z6vSHslgX-CNMtItwWD6Iy5EIP74";
-        $chat_id = "-1001408803296";# Заявки VAG-PIK
-
-        $arr = array(
-            "Заявка с" => " с формы запроса расчета piksp.ru ",
-            "Телефон" => $request->get('phone'),
-            "Марка авто" => $request->get('mark'),
-            "Года авто" => $request->get('year'),
-            "Нужен ли эвакуатор" => $request->get('evakuator'),
-            "Нужны ли запчасти" => $request->get('zapchasti'),
-            "Описание проблемы" => $request->get('problem'),
-            "Со страницы: " => 'https://piksp.ru'.$request->get('url'),
-        );
-        /*Цикл по массиву (собираем сообщение) */
-        $txt = '';
-        foreach($arr as $key => $value) {
-            $txt .= "<b>".$key."</b>: ".htmlspecialchars($value)."\n";
-        }
-        $sendTextToTelegram = file_get_contents("https://api.telegram.org/bot$token/sendMessage?chat_id=$chat_id&parse_mode=html&text=".rawurlencode($txt))."\n";
-        if (!$sendTextToTelegram){
-            return new JsonResponse(['error'=>'<p>Ошибка при отправке в Telegram</p>']);
-        }
-
-        $to = 'info@piksp.ru';
-
-        $email = (new Email())
-            ->from('info@my-side.online')
-            ->to((string)$to)
-            ->subject('Новая заявка с сайта Piksp.ru')
-            ->html('<p>Новая заявка с сайта Piksp.ru</p>
-            <p>Телефон отправителя: ' . $request->get('phone') . '</p>'
-            );
-        $mailer->send($email);
 
 
         return new JsonResponse(['success'=>'<p>Спасибо! Ваша заявка отправлена.</p>']);
