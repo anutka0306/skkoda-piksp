4ecfa4bd7a53db21f55d097fb939b8f03bcb0f2a
popovdmitrij <dima_it@qmotors.ru>
2022-07-14 03:43:06 +0300

    master - test

--- src/Controller/Admin_old/DiagnosticBrandCrudController.php
+++ src/Controller/Admin_old/DiagnosticBrandCrudController.php
@@ -29,7 +29,7 @@ class DiagnosticBrandCrudController extends AbstractCrudController
         return [
             Field::new('id')->onlyOnIndex(),
             AssociationField::new('brand'),
-            AssociationField::new('article')->autocomplete(),
+            AssociationField::new('article')->renderAsNativeWidget()->autocomplete(),
             ImageField::new('image')->setUploadDir('/public/img/diagnostics/')->setBasePath('/img/diagnostics'),
         ];
     }
